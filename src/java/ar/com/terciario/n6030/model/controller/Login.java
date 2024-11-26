8695a96897c2bc1b56c02fc940069917c43021ce
Pablo09824 <pfrau143@gmail.com>
2024-11-25 23:23:40 -0300

    version altualizada pablo

--- src/java/ar/com/terciario/n6030/model/controller/Login.java
+++ src/java/ar/com/terciario/n6030/model/controller/Login.java
@@ -11,6 +11,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 
 @WebServlet(name = "Login", urlPatterns = {"/Login"})
@@ -47,9 +48,9 @@ public class Login extends HttpServlet {
 
         if (u != null ) {
 
-            HttpSession session = request.getSession(true);
+          HttpSession session = request.getSession(true);
 
-            session.setAttribute("usuario", u);
+          session.setAttribute("usuario", u);
 
             String Perfil = u.getPerfil();
 
